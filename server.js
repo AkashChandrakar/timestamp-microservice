7c7ce6696b33fa89e7fc762e297e3292d20f39cd
Akash Chandrakar <akashchandrakar0@gmail.com>
2016-09-11 15:34:51 +0000

    initial commit

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ function unixToNormal(unixDate){
   var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
   ];
-  var d = new Date(unixDate);
-  return monthNames[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear();
+  var date = new Date(unixDate);
+  return monthNames[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
 }
 
 
